bf93530e4ba4dcfb37d35ec05e19512ff9dcaf8e
Mariya-Sharibchenko <m.sharybchanka@arateg.com>
2021-10-06 18:32:39 +0300

    add styles-components

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { helloworld } from 'context';
+import styled from 'styled-components';
 
 interface IProps {
     children?: string;
 }
 
-export const TestComponent = ({ children }: IProps) => <div>{children ? children : helloworld}</div>
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+export const TestComponent = ({ children }: IProps) => {
+  return (
+    <>
+      <div>{children ? children : helloworld}</div>
+      <Button>Styled Button</Button>
+    </>
+  )
+}
