5756e79c62854e2a90792b4d420e521ec0f12596
Mariya-Sharibchenko <m.sharybchanka@arateg.com>
2021-12-01 16:04:25 +0300

    Fix size of initials in StudentImage, fix UserMenu layout, add UserMenuContainer, delite extra 'return' from Filter and Notifications

--- src/molecules/UserMenu/UserMenu.stories.tsx
+++ src/molecules/UserMenu/UserMenu.stories.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { UserMenu, IUserMenuProps } from './UserMenu';
+import { UserMenu, IUserMenuComponentProps } from './UserMenu';
 
-import { UserMenuItems } from 'mock';
+import { StudentArray, UserMenuItems } from 'mock';
 
 export default {
   title: 'TMS/UserMenu',
   component: UserMenu,
 } as Meta;
 
-const Template: Story<IUserMenuProps> = (args) => <UserMenu {...args}/>;
+const Template: Story<IUserMenuComponentProps> = (args) => <UserMenu {...args}/>;
 
 export const DropDownMenu = Template.bind({});
 
 DropDownMenu.args = {
   menuFields: UserMenuItems,
-  userName: 'IBA Group Company '
+  user: StudentArray[0],
+  menuIsOpened: true,
 };
