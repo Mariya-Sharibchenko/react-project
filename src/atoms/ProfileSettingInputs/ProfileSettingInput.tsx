336abb067a4b905179f91cb7eba85929f582fc51
Mariya-Sharibchenko <m.sharybchanka@arateg.com>
2022-01-12 18:04:46 +0300

    create folder

---
+++ src/atoms/ProfileSettingInputs/ProfileSettingInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type ProfileSettingInputTypes = 'email' | 'tel' | 'text' | 'date' | 'url' | 'radio'
+
+export const ProfileSettingInput: React.FC = () => {
+  return (
+    <label>
+      <span></span>
+      <input type=''/>
+    </label>
+  )
+};
