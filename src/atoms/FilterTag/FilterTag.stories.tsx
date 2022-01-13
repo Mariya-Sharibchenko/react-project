6ff6770127d95299f9ae1ff34023a1c04718c6a2
Mariya-Sharibchenko <m.sharybchanka@arateg.com>
2022-01-13 12:04:48 +0300

    Fix filterTag

--- src/atoms/FilterTag/FilterTag.stories.tsx
+++ src/atoms/FilterTag/FilterTag.stories.tsx
@@ -14,6 +14,6 @@ const Template: Story<IFilterTagProps> = (args) => <FilterTag {...args}/>;
 export const FilterTagExample = Template.bind({});
 
 FilterTagExample.args = {
-  text: 'SearchButtonText',
+  text: 'jQuery',
   onClick: action('delete Tag'),
 };
--- src/atoms/FilterTag/FilterTag.tsx
+++ src/atoms/FilterTag/FilterTag.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import { Colors } from 'context';
+import iconClose from 'public/icon-close-for-tag.svg';
+
+const { mainColor } = Colors;
+
 export interface IFilterTagProps {
   text: string,
   onClick: () => void,
 }
 
+const StyledFilterTag = styled.button`
+  cursor: pointer;
+  padding: 6px 36px 6px 15px;
+  background-color: ${mainColor};
+  position: relative;
+  border: none;
+  border-radius: 5px;
+  font-weight: 600;
+  
+  &:after {
+    position: absolute;
+    content: url(${iconClose});
+    width: 24px;
+    height: 24px;
+    top: 2px;
+    right: 6px;
+  }
+`;
+
 export const FilterTag: React.FC<IFilterTagProps> = ({ onClick, text }) => {
   return (
-    <button onClick={onClick}>
+    <StyledFilterTag onClick={onClick}>
       {text}
-    </button>
+    </StyledFilterTag>
   )
 };
